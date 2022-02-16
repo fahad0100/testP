c0d55400b488f77a389e0972d4db6b7babcf5149
TestAuthor <exaple@gmail.com>
2022-02-16 11:32:49 +0300

    sssss

--- Sources/asdasd/asdasd.swift
+++ Sources/asdasd/asdasd.swift
@@ -2,12 +2,20 @@ import UIKit
 
     
    public class myView {
+       var view = UIView()
+       var x = 0
+       var y = 0
+       let sizeScreen = UIScreen.main.bounds
        public init() {
+         
        }
-        let sizeScreen = UIScreen.main.bounds
-       public func addView(x:CGFloat,y:CGFloat) -> UIView{
-            let view = UIView(frame: CGRect(x: x, y: y, width: sizeScreen.midX / 2, height: sizeScreen.midX / 2))
-            view.backgroundColor = .red
+       public func position(x:CGFloat,y:CGFloat){
+            view = UIView(frame: CGRect(x: x, y: y, width: CGFloat(Int(sizeScreen.midX) / 2), height: sizeScreen.midX / 2))
+        }
+       public func backgroundColor(color:UIColor){
+           view.backgroundColor = color
+        }
+       public func addView() -> UIView{
             return view
         }
         

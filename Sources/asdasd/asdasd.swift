0ba4a4d501fac8763a965ba5dd7a10df4715dc0b
TestAuthor <exaple@gmail.com>
2022-02-16 11:09:23 +0300

    sdafsdfasdf

--- Sources/asdasd/asdasd.swift
+++ Sources/asdasd/asdasd.swift
@@ -6,6 +6,8 @@ public struct asdasd {
     }
     
    public class myView {
+       public init() {
+       }
         let sizeScreen = UIScreen.main.bounds
         func addView(x:CGFloat,y:CGFloat) -> UIView{
             let view = UIView(frame: CGRect(x: x, y: y, width: sizeScreen.midX / 2, height: sizeScreen.midX / 2))

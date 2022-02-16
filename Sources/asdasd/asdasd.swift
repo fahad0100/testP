8612d512986c12c7634354f89952103b209af6b0
TestAuthor <exaple@gmail.com>
2022-02-16 11:12:31 +0300

    dasdaSd

--- Sources/asdasd/asdasd.swift
+++ Sources/asdasd/asdasd.swift
@@ -5,7 +5,7 @@ import UIKit
        public init() {
        }
         let sizeScreen = UIScreen.main.bounds
-        func addView(x:CGFloat,y:CGFloat) -> UIView{
+       public func addView(x:CGFloat,y:CGFloat) -> UIView{
             let view = UIView(frame: CGRect(x: x, y: y, width: sizeScreen.midX / 2, height: sizeScreen.midX / 2))
             view.backgroundColor = .red
             return view

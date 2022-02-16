1134ccc182f68c2b64620ca1b4274a64d2e809e7
TestAuthor <exaple@gmail.com>
2022-02-16 11:05:46 +0300

    dddd

--- Sources/asdasd/asdasd.swift
+++ Sources/asdasd/asdasd.swift
@@ -1,6 +1,17 @@
+import UIKit
 public struct asdasd {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    class myView {
+        let sizeScreen = UIScreen.main.bounds
+        func addView(x:CGFloat,y:CGFloat) -> UIView{
+            let view = UIView(frame: CGRect(x: x, y: y, width: sizeScreen.midX / 2, height: sizeScreen.midX / 2))
+            view.backgroundColor = .red
+            return view
+        }
+        
     }
 }
